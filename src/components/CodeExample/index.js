fca23d7713f174084178e3cb371c9efb2e9913d7
Sean Larkin <sean.larkin@cuw.edu>
2017-06-16 00:53:10 -0500

    feat(async): fixed async

--- src/components/CodeExample/index.js
+++ src/components/CodeExample/index.js
@@ -1,5 +1,20 @@
-import AsyncComponent from '@/utilities/asyncComponent';
+import LoadingComponent from '@/components/LoadingComponent';
+// import ErrorComponent from '@/components/Error';
+
+const FancyAsyncComponent = () => ({
+  // The component to load. Should be a Promise
+  component: import('@/components/CodeExample/CodeExample.vue'),
+  // A component to use while the async component is loading
+  loading: LoadingComponent,
+  // A component to use if the load fails
+  error: LoadingComponent,
+  // Delay before showing the loading component. Default: 200ms.
+  delay: 10,
+  // The error component will be displayed if a timeout is
+  // provided and exceeded. Default: Infinity.
+  timeout: 3000,
+});
 
 export default function install(Vue) {
-  Vue.component('code-example', AsyncComponent('CodeExample'));
+  Vue.component('code-example', FancyAsyncComponent);
 }
--- src/utilities/fancyAsyncComponent.js
+++ src/utilities/fancyAsyncComponent.js
@@ -1,17 +1,18 @@
-import AsyncComponent from '@/utilities/asyncComponent';
-import LoadingComponent from '@/components/Loading';
-import ErrorComponent from '@/components/Error';
+import LoadingComponent from '@/components/LoadingComponent';
+// import ErrorComponent from '@/components/Error';
 
-export default componentName => ({
+const FancyAsyncComponent = componentName => ({
   // The component to load. Should be a Promise
-  component: AsyncComponent(componentName)(),
+  component: import(`@/components/${componentName}`),
   // A component to use while the async component is loading
   loading: LoadingComponent,
   // A component to use if the load fails
-  error: ErrorComponent,
+  error: LoadingComponent,
   // Delay before showing the loading component. Default: 200ms.
   delay: 200,
   // The error component will be displayed if a timeout is
   // provided and exceeded. Default: Infinity.
   timeout: 3000,
 });
+
+export default FancyAsyncComponent;
